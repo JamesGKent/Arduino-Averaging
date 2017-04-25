6948c8505363b5cb005d7860ca5e9e1a2925bd78
James <james_g_k@hotmail.co.uk>
2017-04-25 11:22:42 +0100

    fix includes, add array operator [] overload

--- Averaging.h
+++ Averaging.h
@@ -1,3 +1,4 @@
+#include <stdint.h>
 /***********************
 * Generic implementation of Averaging class
 * implements protected variables and methods:
@@ -12,8 +13,8 @@ public:
 	protected :
 	uint16_t _size;
 	uint8_t _bytesize;
-	uint16_t _samples_added = 0;
-	uint16_t _index = 0;
+	uint16_t _samples_added;
+	uint16_t _index;
 	void *_total;
 	void *_samples;
 };
@@ -24,6 +25,7 @@ public:
 	int8_Average(uint16_t array_size);
 	void add_sample(int8_t sample);
 	int8_t get_average();
+	int8_t& operator[] (uint16_t index);
 };
 
 class uint8_Average : public Average
@@ -32,6 +34,7 @@ public:
 	uint8_Average(uint16_t array_size);
 	void add_sample(uint8_t sample);
 	uint8_t get_average();
+	uint8_t& operator[] (uint16_t index);
 };
 
 class int16_Average : public Average
@@ -40,6 +43,7 @@ public:
 	int16_Average(uint16_t array_size);
 	void add_sample(int16_t sample);
 	int16_t get_average();
+	int16_t& operator[] (uint16_t index);
 };
 
 class uint16_Average : public Average
@@ -48,4 +52,5 @@ public:
 	uint16_Average(uint16_t array_size);
 	void add_sample(uint16_t sample);
 	uint16_t get_average();
+	uint16_t& operator[] (uint16_t index);
 };
